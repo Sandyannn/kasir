b203b0d5e04f382fe3b2866767f7d323780a7544
sand <147319678+Sandyannn@users.noreply.github.com>
2025-05-22 20:20:34 +0700

    Update edit.blade.php

--- resources/views/pelanggan/edit.blade.php
+++ resources/views/pelanggan/edit.blade.php
@@ -5,14 +5,23 @@
 
 <form action="{{ route('pelanggan.update', $pelanggan->id) }}" method="POST">
     @csrf
+    @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+
     @method('PUT')
 
     <div class="item">
-        <label for="nama_pelanggan">Nama Pelanggan</label>
-        <input type="text" name="nama_pelanggan" value="{{ $pelanggan->nama_pelanggan }}" required>
+        <label for="nama">Nama</label>
+        <input type="text" name="nama" required>
 
-        <label for="jumlah_transaksi">Jumlah Transaksi</label>
-        <input type="number" name="jumlah_transaksi" value="{{ $pelanggan->jumlah_transaksi }}" min="0">
+        <label for="no_hp">No HP</label>
+        <input type="number" name="no_hp" required>
+
+        <label for="total_transaksi">Total Transaksi</label>
+        <input type="number" name="total_transaksi" min="0">
 
         <button type="submit">Update</button>
     </div>

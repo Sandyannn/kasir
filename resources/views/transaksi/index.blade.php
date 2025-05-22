937d8fe4979527cfd939ad247d8ccd0f0a5b35b4
sand <147319678+Sandyannn@users.noreply.github.com>
2025-05-22 20:21:26 +0700

    Update index.blade.php

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Data Transaksi</h2>
-    <a href="{{ route('transaksi.create') }}" class="btn btn-add">+ Tambah Transaksi</a>
-    <table>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Pelanggan</th>
-                <th>Tanggal Transaksi</th>
-                <th>Total Harga</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($transaksis as $index => $transaksi)
-                <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $transaksi->pelanggan->nama_pelanggan }}</td>
-                    <td>{{ $transaksi->tanggal_transaksi->format('d-m-Y') }}</td>
-                    <td>Rp{{ number_format($transaksi->total_harga, 0, ',', '.') }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<h2>Data Transaksi</h2>
+<a href="{{ route('transaksi.create') }}" class="btn btn-add">+ Tambah Transaksi</a>
+<table>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama Pelanggan</th>
+            <th>Tanggal Transaksi</th>
+            <th>Total Harga</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($transaksis as $index => $transaksi)
+        <tr>
+            <td>{{ $index + 1 }}</td>
+            <td>{{ optional($transaksi->pelanggan)->nama?? '-' }}</td>
+            <td>{{ $transaksi->tanggal_transaksi->format('d-m-Y') }}</td>
+            <td>Rp{{ number_format($transaksi->total_harga, 0, ',', '.') }}</td>
+            <td>
+                <a href="{{ route('transaksi.show', $transaksi->id) }}" class="btn btn-edit">Lihat Riwayat</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection

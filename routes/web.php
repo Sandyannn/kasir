f9b6af3ed2dd1270f1f6849febf3c2492ad112d9
sand <147319678+Sandyannn@users.noreply.github.com>
2025-05-22 20:17:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
+use \App\Http\Controllers\EventController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\NotaController;
 
 Route::get('/', function () {
     return redirect('/transaksi');
 });
 Route::resource('barang', BarangController::class);
+Route::resource('event', EventController::class);
 Route::resource('pelanggan', PelangganController::class);
 Route::resource('transaksi',TransaksiController::class);
-Route::get('/nota/{id}', [\App\Http\Controllers\TransaksiController::class, 'show'])->name('nota.show');
+Route::get('/nota/{id}', [NotaController::class, 'show'])->name('nota.show');
 Route::get('/nota/{id}/cetak', [TransaksiController::class, 'cetakNota'])->name('nota.cetak');

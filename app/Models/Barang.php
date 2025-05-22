994feac6f1000e586a01128c3dcd9bc4441b7499
sand <147319678+Sandyannn@users.noreply.github.com>
2025-05-22 20:23:52 +0700

    Update Barang.php

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -10,11 +10,33 @@ class Barang extends Model
     use HasFactory;
 
     protected $fillable = [
-        'nama_barang', 'harga', 'stok'
+        'nama_barang',
+        'harga',
+        'stok'
     ];
 
     public function transaksiDetails()
     {
         return $this->hasMany(TransaksiDetail::class);
+    }
+
+public function event()
+{
+    return $this->hasOne(Event::class);
+}
+
+    public function eventAktif()
+    {
+        return $this->hasOne(Event::class)
+            ->where(function ($query) {
+                $today = now()->toDateString();
+                $query->whereNull('tanggal_mulai')
+                    ->orWhere('tanggal_mulai', '<=', $today);
+            })
+            ->where(function ($query) {
+                $today = now()->toDateString();
+                $query->whereNull('tanggal_selesai')
+                    ->orWhere('tanggal_selesai', '>=', $today);
+            });
     }
 }
